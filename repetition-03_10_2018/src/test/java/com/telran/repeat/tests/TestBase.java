96f53392f8ae15182a9e417fc787320b0aa04f7e
orewan <b.proweb100@gmail.com>
2018-12-04 11:36:57 +0200

    ch

--- repetition-03_10_2018/src/test/java/com/telran/repeat/tests/TestBase.java
+++ repetition-03_10_2018/src/test/java/com/telran/repeat/tests/TestBase.java
@@ -10,7 +10,8 @@ import org.testng.annotations.BeforeSuite;
 public class TestBase {
 
   //protected static ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
-  protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+  protected static ApplicationManager app =
+          new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite
   public void setUp(){
